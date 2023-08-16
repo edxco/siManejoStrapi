47628a46b8bcfc742c40297e63dc23e776071720
eddbac <eduardo.baeza@asymm.com>
2023-08-16 15:48:45 -0600

    Initial strapi config

---
+++ src/api/escuela/routes/escuela.ts
@@ -0,0 +1,7 @@
+/**
+ * escuela router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::escuela.escuela');
---
+++ src/api/escuela/services/escuela.ts
@@ -0,0 +1,7 @@
+/**
+ * escuela service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::escuela.escuela');
