58b4ea9df63dde9944c86f0a7a287bb00ffd42e5
Edxco <eduardo.baeza@asymm.com>
2023-11-13 13:22:32 -0600

    New fields for escuela collectionType

--- config/database.ts
+++ config/database.ts
@@ -7,7 +7,12 @@ export default ({ env }) => ({
       database: env("DATABASE_NAME", "simanejostrapi"),
       user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
+    //   ssl: {
+    //     rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+    //   },
+    // },
+    // options: {
+    //   ssl: env.bool("DATABASE_SSL", true),
     },
-    useNullAsDefault: true,
   },
 });
